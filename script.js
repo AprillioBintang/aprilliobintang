09b5769a90f79b0ac84949040000c8d6ecdd6fc5
AprillioBintang <152962068+AprillioBintang@users.noreply.github.com>
2023-12-05 15:22:21 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const wrapper = document.querySelector(".wrapper");
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+
+yesBtn.addEventListener("click", () => {
+    question.innerHTML = "Aaaaa, I like you too😄😄";
+    gif.src = "https://i.pinimg.com/originals/c7/ad/38/c7ad385c30ea551a2b9a12d21169f5be.gif";
+});
+
+noBtn.addEventListener("click", () => {
+    const noBtnRect = noBtn.getBoundingClientRect();
+    const maxX = window.innerWidth - noBtnRect.width;
+    const maxY = window.innerHeight - noBtnRect.height;
+   
+    const randomX = Math.floor(Math.random() * maxX);
+    const randomY = Math.floor(Math.random() * maxY);
+    
+    noBtn.style.left = randomX + "px";
+    noBtn.style.top = randomY + "px"; 
+});
